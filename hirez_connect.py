1d912a25035a409ab84c8d928dcf8536b5dfd55f
Dan Martz <djmartz77@gmail.com>
2018-09-03 21:56:30 -0500

    Add SMITE API to Table mappings.xlsx, add SMITE_Base_Data.py, create functions construct_api_url and make_api_call. refactor all api calls to use construct_api_url and make_api_call.

--- hirez_connect.py
+++ hirez_connect.py
@@ -1,6 +1,14 @@
 # hirez_connect.py is for establishing a session with the
 # Smite/Paladins/Realm Royale API and determining how much utilization
 # is left
+# Could create a function which returns a list of the parameters needed for each call. Each dictionary key is a API call name, such as 'createsession'
+# { 'createsession' : ['dev_id','sig','timestamp']} which is used to construct the url for the createsession API call
+# here are the functions
+# Alternatively, I could just return a link to the API documentation, and people can look it up on their own.
+
+
+# Also, perhaps this all needs to be remodeled as an object with getters and setters for game, platform, response_format, etc.
+
 
 from urllib.request import urlopen
 import urllib.error
@@ -11,7 +19,9 @@ import time
 dev_id = '2424'
 auth_key = 'CC2DB36A40E9483D9EDD13AAC5CE566A'
 
-PACKAGE_NAME = 'SMITE Duel Helper'
+HIREZ_API_DOCUMENTATION='https://www.hirezstudios.com/wp-content/themes/hi-rez-studios/pdf/smite-api-developer-guide.pdf'
+HIREZ_API_VERSION = {'SMITE': '3.24.0.29040', 'Paladins':'0.0.41.19562'} # PC API Versions, PS4 and XBOX end differently
+PACKAGE_NAME = 'Hirez Connect for Python'
 REPOSITORY_HOME = 'http://github.com/djmartz77/SMITEDuelHelper'
 DEFAULT_RESPONSE_FORMAT = 'json'
 DEBUG_MODE = True
@@ -30,6 +40,21 @@ ENDPOINTS = {
 					'PS4' : 'http://api.ps4.paladins.com/paladinsapi.svc'
 				}
 			}
+
+QUEUES = {
+			"SMITE": {
+				"Ranked":
+				{
+					"Duel" : "440",
+					"Joust": "450",
+					"Conquest": "451",
+				},
+				"Casual":
+				{
+					"Assault": "445",
+				},
+			}
+		 }
 
 def validate_endpoint_settings(game, platform, response_format):
 	if response_format.lower() == 'xml' :
@@ -61,53 +86,155 @@ def base_url(game, platform):
 	return url
 
 
-def ping (game, platform='PC', response_format='json'):
-	if validate_endpoint_settings(game, platform, response_format):
-		url = base_url(game, platform) + '/ping' + response_format.lower()
-		doc = urlopen(url)
-		html = doc.read()
-		return html
-	else:
-		return None
+def construct_api_url(api_call, api_params, game='SMITE', platform='PC', response_format='json'):
+	# api_params_list is all the values needed for the call, starting with the api_name, which are simply put together in order,
+	# I think I just wrote the whole function
+	url = base_url(game, platform)
+	api_params = [api_call + response_format] + api_params
+	if 'developerId' in api_params:
+		api_params[api_params.index('developerId')] = dev_id
+	if 'signature' in api_params:
+		api_params[api_params.index('signature')] = get_signature(api_call, get_timestamp())
+	if 'timestamp' in api_params:
+		api_params[api_params.index('timestamp')] = get_timestamp()
 
-def get_timestamp():
-	return time.strftime("%Y%m%d%H%M%S", time.gmtime())
+	for param in api_params:
+		url = url + '/' + param
+	return url
 
 def get_signature(method_name, timestamp):
 	# returns a signature hash
 	signature = hashlib.md5()
-	hash_string = dev_id + "createsession" + auth_key + timestamp
+	hash_string = dev_id + method_name + auth_key + timestamp
 	signature.update(hash_string.encode())
-	print(signature.hexdigest())
+	# print(signature.hexdigest())
 	return signature.hexdigest()
 
-def testsession(session):
-	# A means of validating that a session is established
-	return None #placeholder
+def  make_api_call(api_call, api_params = [], game='SMITE', platform='PC', response_format='json'):
+	if validate_endpoint_settings(game, platform, response_format) is not True:
+		return None # perhaps an error is more appropriate, but None will do for now
+
+	url = construct_api_url(api_call, api_params, game, platform, response_format)
+	print('make_api_call url', url)
+	try:
+		doc = urlopen(url)
+	except: # I should catch the http response error here
+		return None
+	response = doc.read()
+	return response
+
+def get_timestamp():
+	#returns timestamp formatted for Hi-Rez API, yyyyMMddHHmmss
+	return time.strftime("%Y%m%d%H%M%S", time.gmtime())
+
+def ping (game, platform='PC', response_format='json'):
+	# A quick way of validating access to the Hi-Rez API
+	response = make_api_call('ping', [], game, platform, response_format)
+	return response
 
 def createsession(game, platform='PC', response_format='json'):
 	# A required step to Authenticate the developerId/signature for further API use
-	method_name = "createsession"
-	UTC_timestamp = get_timestamp()
-	signature = get_signature(method_name, UTC_timestamp)
-	url = base_url(game, platform) + '/' + method_name + response_format.lower() + '/' + dev_id + '/' + signature + '/' + UTC_timestamp
+	params_list = ['developerId', 'signature', 'timestamp']
+	result = make_api_call('createsession', params_list, game, platform, response_format)
+	if response_format.lower() == 'json':
+		session = json.loads(result)['session_id']
+	else:
+		session = None
+	return session # will return a session or None
 
-	try:
-		doc = urlopen(url)
-	except:
-		return None
-	
-	json_string = doc.read()
-	session_id = json.loads(json_string)['session_id']
-	return session_id # will return a session or None
+def testsession(session, game, platform='PC', response_format='json'):
+	# A means of validating that a session is established
+	params_list = ['developerId', 'signature', session, 'timestamp']
+	response = make_api_call('testsession', params_list, game, platform, response_format)
+	return response
 
-def gethirezserverstatus(session):
-	# Function returns UP/DOWN status for the primary gmae/platform environments. Data is cached once a minute
-	return None # will return status object of some kind
 
-def getdataused(session):
-	# Returns API Developer daily usage limits and the current status against those limits
-	return None # will return status object of some kind
+def gethirezserverstatus(session, game, platform='PC', response_format='json'):
+	# Function returns UP/DOWN status for the primary game/platform environments. Data is cached once a minute
+	params_list = ['developerId', 'signature', session, 'timestamp']
+	response = make_api_call('gethirezserverstatus', params_list, game, platform, response_format)
+	if response_format.lower() == 'json':
+		status = json.loads(response)[0]['status']
+	else:
+		status = None
+	return status
 
-print(ping('SMITE','PC','json'))
-print(createsession('SMITE', 'PC', 'json'))
+def getdataused(session, game, platform='PC', response_format='json'):
+	params_list = ['developerId', 'signature', session, 'timestamp']
+	response = make_api_call('getdataused', params_list, game, platform, response_format)
+	return response
+
+#===================================
+# Data retrieval methods follow
+#===================================
+
+def getgods(session, game, platform='PC', response_format='json', language='1'):
+	# Returns all Gods and their various attributes
+	params_list = ['developerId', 'signature', session, 'timestamp', language]
+	response = make_api_call('getgods', params_list, game, platform, response_format)
+	return response
+
+def getitems(session, game, platform='PC', response_format='json', language='1'):
+	# Returns all Items and their various attributes
+	params_list = ['developerId', 'signature', session, 'timestamp', language]
+	response = make_api_call('getitems', params_list, game, platform, response_format)
+	return response
+
+def getmatchdetails(session, game, match_id, platform='PC', response_format='json'):
+	# Returns the statistics for a particular completed match
+	params_list = ['developerId', 'signature', session, 'timestamp', match_id]
+	response = make_api_call('getmatchdetails', params_list, game, platform, response_format)
+	return response
+
+def getleagueleaderboard(session, game, platform='PC', response_format='json', queue=QUEUES['SMITE']['Ranked']['Duel'], tier='27', season='5'):
+	# Returns the top players for a particular league (as indicated by the queue/tier/season parameters)
+	params_list = ['developerId', 'signature', session, 'timestamp', queue, tier, season]
+	response = make_api_call('getleagueleaderboard', params_list, game, platform, response_format)
+	return response
+
+def getmatchidsbyqueue(session, game, platform='PC', response_format='json', queue=QUEUES['SMITE']['Ranked']['Duel'], date='20180831', hour='0'):
+	# Lists all Match IDs for a particular match queue for the given time period as indicated by date and hour.
+	# If hour is -1 it returns the entire day's Match IDs
+	params_list = ['developerId', 'signature', session, 'timestamp', queue, date, hour]
+	response = make_api_call('getmatchidsbyqueue', params_list, game, platform, response_format)
+	return response
+
+
+
+print(ping('SMITE'))
+print("")
+session = createsession('SMITE', 'PC', 'json')
+print(session)
+# session = '8F0C4ACDC567485C93180F141F274CD1'
+session_active = testsession(session, 'SMITE', 'PC', 'json')
+print(session_active)
+print("")
+
+data_used = getdataused(session, 'SMITE', 'PC', 'json')
+print(data_used)
+print("")
+
+server_status = gethirezserverstatus(session, 'SMITE', 'PC', 'json')
+print(server_status)
+print("")
+
+gods = getgods(session, 'SMITE', 'PC', 'json')
+# print(gods)
+print("")
+
+items = getitems(session, 'SMITE', 'PC', 'json')
+# print(items)
+print("")
+
+leaderboard = getleagueleaderboard(session, 'SMITE', 'PC', 'json', QUEUES['SMITE']['Ranked']['Duel'], '26', '5')
+# print(items)
+print("")
+
+matchids = getmatchidsbyqueue(session, 'SMITE', 'PC', 'json', QUEUES['SMITE']['Ranked']['Duel'], '20180831', '-1')
+# print(items)
+print("")
+
+match_id = '416810340'
+match = getmatchdetails(session, 'SMITE', match_id)
+# print(match)
+print("")
